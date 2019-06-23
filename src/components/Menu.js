9daf484ac30dd88842c7596755666412d0cf8733
lucymulligan <lucymulligan@hotmail.com>
2019-06-23 13:37:15 +0100

    Added some carousel pictures

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,6 +8,7 @@ import Col from 'react-bootstrap/Col';
 import AccordionCollapse from "react-bootstrap/AccordionCollapse";
 
 
+
 class Menu extends Component {
   render() {
     return (
@@ -23,13 +24,13 @@ class Menu extends Component {
 
         <div className="row">
           <div className="col-12">
-            <Accordion>
+            <Accordion className="Accordion1">
               <Card.Header>
-                <Accordion.Toggle as={Button} variant="light" eventKey="1" className="d-flex align-items-end flex-column">
+                <Accordion.Toggle as={Button} variant="light" eventKey="0" className="d-flex align-items-end flex-column">
                   View Menu
                   </Accordion.Toggle>
               </Card.Header>
-              <AccordionCollapse eventKey="1">
+              <AccordionCollapse eventKey="0">
                 <Card>
                   <Card.Body>Hello! I'm another body</Card.Body>
                   <Card.Footer>
@@ -45,67 +46,69 @@ class Menu extends Component {
                       <Accordion.Toggle as={Button} variant="secondary" type="submit" eventKey="1" className="btn-block mb-3" >
                         Book Now
                   </Accordion.Toggle>
-                      <AccordionCollapse eventKey="1">
-                        <Card>
-                          <Card.Body>
-                            <Form>
-                              <Row>
-                                <Col>
-                                  <Form.Control placeholder="First name" />
-                                </Col>
-                                <Col>
-                                  <Form.Control placeholder="Last name" />
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col>
-                                  <Form.Group controlId="formGroupEmail">
-                                    <Form.Label></Form.Label>
-                                    <Form.Control type="email" placeholder="Email Address" />
-                                  </Form.Group>
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col>
-                                  <Form.Group controlId="formGroupEmail">
-                                    <Form.Control type="email" placeholder="Address 1" />
-                                  </Form.Group>
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col>
-                                  <Form.Group controlId="formGroupEmail">
-                                    <Form.Control type="email" placeholder="Address 2" />
-                                  </Form.Group>
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col>
-                                  <Form.Control placeholder="City" />
-                                </Col>
-                                <Col>
-                                  <Form.Control placeholder="Postcode" />
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col>
-                                  <Form.Group controlId="formGroupEmail">
-                                    <Form.Label></Form.Label>
-                                    <Form.Control type="email" placeholder="Contact Number" />
-                                  </Form.Group>
-                                </Col>
-                              </Row>
-                            </Form>
-                          </Card.Body>
-                          <Card.Footer>
-                            <Form.Group controlId="exampleForm.ControlSelect1">
-                              <Button variant="secondary" type="submit" className="btn-block mb-3" >
-                                Confirm
+                      <Accordion defaultActiveKey="0" className="Accordion2">
+                        <AccordionCollapse eventKey="1">
+                          <Card>
+                            <Card.Body>
+                              <Form>
+                                <Row>
+                                  <Col>
+                                    <Form.Control placeholder="First name" />
+                                  </Col>
+                                  <Col>
+                                    <Form.Control placeholder="Last name" />
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col>
+                                    <Form.Group controlId="formGroupEmail">
+                                      <Form.Label></Form.Label>
+                                      <Form.Control type="email" placeholder="Email Address" />
+                                    </Form.Group>
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col>
+                                    <Form.Group controlId="formGroupEmail">
+                                      <Form.Control type="email" placeholder="Address 1" />
+                                    </Form.Group>
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col>
+                                    <Form.Group controlId="formGroupEmail">
+                                      <Form.Control type="email" placeholder="Address 2" />
+                                    </Form.Group>
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col>
+                                    <Form.Control placeholder="City" />
+                                  </Col>
+                                  <Col>
+                                    <Form.Control placeholder="Postcode" />
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col>
+                                    <Form.Group controlId="formGroupEmail">
+                                      <Form.Label></Form.Label>
+                                      <Form.Control type="email" placeholder="Contact Number" />
+                                    </Form.Group>
+                                  </Col>
+                                </Row>
+                              </Form>
+                            </Card.Body>
+                            <Card.Footer>
+                              <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Button variant="secondary" type="submit" className="btn-block mb-3" >
+                                  Confirm
                           </Button>
-                            </Form.Group>
-                          </Card.Footer>
-                        </Card>
-                      </AccordionCollapse>
+                              </Form.Group>
+                            </Card.Footer>
+                          </Card>
+                        </AccordionCollapse>
+                      </Accordion>
                     </Form.Group>
                   </Card.Footer>
                 </Card>
@@ -114,6 +117,8 @@ class Menu extends Component {
           </div>
         </div>
       </Card>
+
+
 
 
 
