3b092b995de1db6700d1ada3ebb90368fbf0ade8
lucymulligan <lucymulligan@hotmail.com>
2019-06-25 20:38:57 +0100

    looking at steps text

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,64 +1,48 @@
 import React, { Component } from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import carousel1 from './Images/carousel1.jpg';
+import carousel2 from './Images/carousel2.jpg';
+import carousel3 from './Images/carousel3.jpg';
+import carousel4 from './Images/carousel4.jpg';
+import carousel5 from './Images/carousel5.jpg';
+
 
 class Steps extends Component {
   render() {
     return (
       <Carousel className="mb-3 mt-3">
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1556909114-a1d34f47412c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-          alt="First slide"
-          height="550px"
-
-        />
+      <img src={carousel1} alt='logo' className='carousel1' height="550px" />
         <Carousel.Caption>
-          <h1>Select cuisine, budget and date</h1>
+          <h4>Select cuisine, budget and date</h4>
          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/450x225"
-          alt="First slide"
-        />
+      <img src={carousel2} alt='logo' className='carousel2' height="550px" />
         <Carousel.Caption>
-          <h1>Find your perfect menu</h1>
+          <h4>Find your perfect menu</h4>
           
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/450x225"
-          alt="First slide"
-        />
+      <img src={carousel3} alt='logo' className='carousel3' height="550px" />
         <Carousel.Caption>
-          <h1>Let us know who many guests to cater for</h1>
+          <h4>Let us know who many guests to cater for</h4>
           
         </Carousel.Caption>
       </Carousel.Item>
        <Carousel.Item>
-     <img
-       className="d-block w-100"
-       src="https://via.placeholder.com/450x225"
-       alt="First slide"
-     />
+       <img src={carousel4} alt='logo' className='carousel4' height="550px" />
      <Carousel.Caption>
-       <h1>Confirm booking</h1>
+       <h4>Confirm booking</h4>
        
      </Carousel.Caption>
    </Carousel.Item>
    <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/450x225"
-          alt="First slide"
-        />
+   <img src={carousel5} alt='logo' className='carousel5' height="550px"/>
         <Carousel.Caption>
-          <h1>Sit back, relax, we'll take it from here</h1>
+          <h4>Sit back, relax, we'll take it from here</h4>
          
         </Carousel.Caption>
       </Carousel.Item>
